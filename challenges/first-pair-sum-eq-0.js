7b5866e5bb4c8cdd2e101221a91dc63c651411a0
= <=>
2020-05-11 23:44:10 -0300

    feat: add `first-pair-sum-eq-0` challenge

--- challenges/first-pair-sum-eq-0.js
+++ challenges/first-pair-sum-eq-0.js
@@ -2,16 +2,28 @@
  * @description
  * First pair of an array who sums equals 0 
  * The array is sorted from the lower to the higher 
+ * 
+ * 
  * @example [-3, -2, 0, 2, 4] -> 2
  */
 
+/**
+ * Duas coisas que aprendi hoje, 
+ * - Exemplo da utilização de dois ponteiros em um array (left, right -> left < right)
+ * - Controle dos valores de um for loop dentro dele passando `;` 
+ */
+
 const firstPair = (arr) => {
-    for (let i = 0, j = arr.length - 1; i < arr.length / 2 || j >= arr.length / 2; i++, j--) {
-        if (!(arr[i] + arr[j])) {
-            return arr[j]
+    for (let left = 0, right = arr.length - 1; left < right;) {
+        const sum = arr[left] + arr[right]
+        if (!sum) {
+            return Math.abs(arr[left])
+        } else if (sum < 0) {
+            left++
+        } else {
+            right--
         }
     }
-    return false
 }
 
-console.log(firstPair([-3, -2, 0, 1, 2, 3]))
+module.exports = firstPair
