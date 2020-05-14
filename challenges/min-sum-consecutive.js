125875c9d72971c3dee7111c5cf426ea0f651a91
Fabricio Parola <fabricio.parola@gmail.com>
2020-05-14 02:48:08 -0300

    fix: PORRA ACABEI ESSA MERDA 2 HORAS 18 LINHAS INFERNOOOOOOO!!!! AAAAAAAAAAAAAAAAAAAA

--- challenges/min-sum-consecutive.js
+++ challenges/min-sum-consecutive.js
@@ -4,60 +4,19 @@
  * [1, 5, 7, 3, 5, 9], 3 -> 3 + 5 + 9 =  17
  */
 
-/**
-*  for (let i = 0, j = 0; i < arr.length;) {
-       // console.log(sum)
-       if (sum < num && j < arr.length) {
-           console.log('aa')
-           sum += arr[j++]
-           continue
-       } else if (i < j) {
-           result = j
-           sum -= arr[i++]
-           console.log(sum, arr[i++])
-       }
-   }
-*/
-
-// const minSumConsecutive = (arr, num) => {
-//     let sum = 0,
-//         result = 0
-
-//     for (let i = 0, j = 0; i < arr.length;) {
-//         console.log('j', j, 'i', i, 'jV', arr[j], 'iV', arr[i], 'sum', sum, 'result', result)
-//         if (j < arr.length) {
-//             sum += arr[j++]
-//         } else if (i < j) { //sum >= num
-//             result = j - i
-//             sum -= arr[i++]
-//         }
-//     }
-
-//     console.log('sum', sum)
-//     console.log('result', result)
-//     return result + 1
-// }
-
-
-
-// minSumConsecutive([2, 0, 1], 3)
-
 const minSumConsecutive = (arr, num) => {
     if (!arr.length) {
         return 0
     }
 
     let sum = 0,
-        result
+        result = Infinity
 
     for (let i = 0, j = 0; i < arr.length;) {
-        if (j < arr.length) {
-            console.log(sum)
+        if (sum < num && j < arr.length) {
             sum += arr[j++]
         } else if (sum >= num) {
-            console.log(j)
-            break
-            result = j - i
+            result = Math.min(result, j - i)
             sum -= arr[i++]
         } else {
             break
@@ -65,8 +24,7 @@ const minSumConsecutive = (arr, num) => {
 
     }
 
-    console.log(result)
-    return result
+    return result === Infinity ? 0 : result
 }
 
 
