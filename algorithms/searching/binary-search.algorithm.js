d8647b6422b0029508ba64e1da2bd20227f2c03f
Fabricio Parola <fabricio.parola@gmail.com>
2020-05-20 21:20:56 -0300

    fix: binary search alg

--- algorithms/searching/binary-search.algorithm.js
+++ algorithms/searching/binary-search.algorithm.js
@@ -2,6 +2,7 @@ const binaryNumbersSearch = (sortedArr, value) => {
     if (!sortedArr.length) {
         return -1
     }
+
     const middleIndex = Math.floor(sortedArr.length / 2)
     const middleValue = sortedArr[middleIndex]
 
@@ -15,5 +16,4 @@ const binaryNumbersSearch = (sortedArr, value) => {
     return searchResult !== -1 ? middleIndex + 1 + searchResult : -1
 }
 
-const r = binaryNumbersSearch([10, 20, 30, 50, 55], 56)
 module.exports = binaryNumbersSearch
